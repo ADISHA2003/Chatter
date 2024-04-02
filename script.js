969185672c6ff54e17deb661e7fce387ad97b571
Aditya Sharma <157511091+ADISHA2003@users.noreply.github.com>
2024-04-02 18:58:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ function sendMessage() {
     // Add class to indicate chatting has started
     document.getElementById("chat-container").classList.add("chatting");
 
-    // Add class to indicate searching has started
-    document.getElementById("chat-container").classList.add("searching");
-
     addUserMessage(userInput);
     document.getElementById("user-input").value = "";
 
@@ -19,7 +16,7 @@ function sendMessage() {
     startTypingAnimation();
 }
 
-function search() {
+function startSearch() {
     var logo = document.getElementById("logo");
     if (logo) {
         logo.style.opacity = 0;
