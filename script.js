9e8bd9c2233a61bb899e3c0e9002a037a187d651
Aditya Sharma <157511091+ADISHA2003@users.noreply.github.com>
2024-04-02 22:36:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -245,7 +245,7 @@ function startTypingAnimation() {
         } else {
             clearInterval(typingInterval); // Stop the typing animation when message is complete
         }
-    }, 20); // Adjust typing speed as needed
+    }, 5); // Adjust typing speed as needed
 }
 
     // Focus on input field when the page loads
