b6cbc05ea13fd335c0431a32f3f5f539a2853fc1
Aditya Sharma <157511091+ADISHA2003@users.noreply.github.com>
2024-04-29 21:53:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -416,7 +416,7 @@ displayKeywords();
 
 document.addEventListener('DOMContentLoaded', function() {
     const text = "Chatter (Model - 1.0)";
-    const typingSpeed = 80;
+    const typingSpeed = 50;
     let index = 0;
     const headerStrong = document.querySelector('header');
 
