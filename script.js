2771aac253d03d853d4d7a52301e37bc84d4e5bc
Aditya Sharma <157511091+ADISHA2003@users.noreply.github.com>
2024-05-22 00:41:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ function displayResults(data, maxResults) {
                 linkElement.href = result.link;
                 linkElement.target = "_blank";
                 linkElement.textContent = "Read More";
-                linkElement.style.color = "#0000ff";
+                linkElement.style.color = "#00BFFF";
  linkElement.style.textDecoration = "none";
  messageElement.appendChild(linkElement);
 
