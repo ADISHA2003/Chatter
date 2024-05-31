061d38923a477d819108b05eabca60182ec3cb5d
Aditya Sharma <157511091+ADISHA2003@users.noreply.github.com>
2024-05-31 17:43:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -369,11 +369,11 @@ function displayKeywords() {
 displayKeywords();
 
 document.addEventListener('DOMContentLoaded', function() {
-    const text = "Chatter-1.0";
+    const text = "Chatter - Beta";
     const initialDelay = 0; // 1 second delay before typing starts
     let index = 0;
-    const typingSpeedMin = 60; // Minimum typing speed (milliseconds per character)
-    const typingSpeedMax = 80; // Maximum typing speed (milliseconds per character)
+    const typingSpeedMin = 80; // Minimum typing speed (milliseconds per character)
+    const typingSpeedMax = 100; // Maximum typing speed (milliseconds per character)
     const headerStrong = document.querySelector('header');
 
     if (headerStrong) {
